2a91ee1c24b8b3515e769b8d441a623de7aa8c0f
Kevin J Gomez <kjgmez1@gmail.com>
2018-11-11 19:33:06 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,7 +1,10 @@
-def self.reset_all
-  self.all.clear
-end
+class Memorable
 
-def self.count
-  self.all.count
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
 end
